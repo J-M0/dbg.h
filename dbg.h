af0d25d0bc585e1fc8a3770b86d8b883c1296d59
James Morris <jamesmorris2@gmail.com>
2023-11-07 14:07:44 -0500

    Unify dbg and dbgfmt implementations

--- dbg.h
+++ dbg.h
@@ -4,34 +4,35 @@
 #include <stdio.h>
 #include <wchar.h>
 
-#define dbgfmt(expr, format) fprintf(stderr, "[dbg] "#expr" = "#format"\n", (expr))
-
-#define dbg(expr) do { \
+#define _dbgimpl(expr, format) do { \
 	fputs("[dbg] "#expr" = ", stderr); \
-	fprintf(stderr, \
-		_Generic((expr), \
-			signed char: "%hhd\n", \
-			short: "%hd\n", \
-			int: "%d\n", \
-			long: "%ld\n", \
-			long long: "%lld\n", \
-			\
-			unsigned char: "%hhu\n", \
-			unsigned short: "%hu\n", \
-			unsigned int: "%u\n", \
-			unsigned long: "%lu\n", \
-			unsigned long long: "%llu\n", \
-			\
-			float: "%f\n", \
-			double: "%f\n", \
-			long double: "%Lf\n", \
-			\
-			char*: "%s\n", \
-			wchar_t*: "%ls\n", \
-			void*: "%p\n" \
-		), \
-		(expr) \
-	); \
+	fprintf(stderr, (format), (expr)); \
 } while(0)
+
+#define dbgfmt(expr, format) _dbgimpl(expr, #format"\n")
+
+#define dbg(expr) _dbgimpl(expr, \
+	_Generic((expr), \
+		signed char: "%hhd\n", \
+		short: "%hd\n", \
+		int: "%d\n", \
+		long: "%ld\n", \
+		long long: "%lld\n", \
+		\
+		unsigned char: "%hhu\n", \
+		unsigned short: "%hu\n", \
+		unsigned int: "%u\n", \
+		unsigned long: "%lu\n", \
+		unsigned long long: "%llu\n", \
+		\
+		float: "%f\n", \
+		double: "%f\n", \
+		long double: "%Lf\n", \
+		\
+		char*: "%s\n", \
+		wchar_t*: "%ls\n", \
+		void*: "%p\n" \
+	) \
+)
 
 #endif // DBG_H
