f8dcf2aefc9ab6c34ea083651eb23bf58646fa09
James Morris <jamesmorris2@gmail.com>
2023-11-07 11:18:28 -0500

    Make dbg generic over the type of the expression and keep old behavior in dbgfmt

--- dbg.h
+++ dbg.h
@@ -2,7 +2,36 @@
 #define DBG_H
 
 #include <stdio.h>
+#include <wchar.h>
 
-#define dbg(expr, format) fprintf(stderr, "[dbg] "#expr" = "#format"\n", (expr))
+#define dbgfmt(expr, format) fprintf(stderr, "[dbg] "#expr" = "#format"\n", (expr))
+
+#define dbg(expr) do { \
+	fputs("[dbg] "#expr" = ", stderr); \
+	fprintf(stderr, \
+		_Generic((expr), \
+			signed char: "%hhd\n", \
+			short: "%hd\n", \
+			int: "%d\n", \
+			long: "%ld\n", \
+			long long: "%lld\n", \
+			\
+			unsigned char: "%hhu\n", \
+			unsigned short: "%hu\n", \
+			unsigned int: "%u\n", \
+			unsigned long: "%lu\n", \
+			unsigned long long: "%llu\n", \
+			\
+			float: "%f\n", \
+			double: "%f\n", \
+			long double: "%Lf\n", \
+			\
+			char*: "%s\n", \
+			wchar_t*: "%ls\n", \
+			void*: "%p\n" \
+		), \
+		(expr) \
+	); \
+} while(0)
 
 #endif // DBG_H
---
+++ test.c
@@ -0,0 +1,48 @@
+#include "dbg.h"
+
+int main(void) {
+	signed char sc = 1;
+	dbg(sc);
+	short s = 2;
+	dbg(s);
+	int i = 3;
+	dbg(i);
+	long l = 4;
+	dbg(l);
+	long long ll = 5;
+	dbg(ll);
+
+	unsigned char uc = 6;
+	dbg(uc);
+	unsigned short us = 7;
+	dbg(us);
+	unsigned int ui = 8;
+	dbg(ui);
+	unsigned long ul = 9;
+	dbg(ul);
+	unsigned long long ull = 10;
+	dbg(ull);
+
+	float f = 11.0f;
+	dbg(f);
+	double d = 12.0;
+	dbg(d);
+	long double ld = 13.0;
+	dbg(ld);
+
+	char* cp = "14";
+	dbg(cp);
+	wchar_t* wp = L"15";
+	dbg(wp);
+	void* vp = cp;
+	dbg(vp);
+
+	struct { int x; int y; } point = {100, 200};
+
+	dbg(point.x);
+	dbg(point.y);
+
+	dbgfmt(1 + 1, %Lf);
+
+	return 0;
+}
