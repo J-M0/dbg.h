3609eff03cd8d891c8b2bd12fd772ca7a681e610
James Morris <jamesmorris2@gmail.com>
2023-11-08 18:10:10 -0500

    Rename interal macro to be compliant with standard C

--- dbg.h
+++ dbg.h
@@ -6,14 +6,14 @@
 #include <stdio.h>
 #include <wchar.h>
 
-#define _dbgimpl(expr, format) do { \
+#define dbg_impl_(expr, format) do { \
 	fprintf(stderr, "[dbg] "__FILE__":%d: "#expr" = ", __LINE__); \
 	fprintf(stderr, (format), (expr)); \
 } while(0)
 
-#define dbgfmt(expr, format) _dbgimpl(expr, #format"\n")
+#define dbgfmt(expr, format) dbg_impl_(expr, #format"\n")
 
-#define dbg(expr) _dbgimpl(expr, \
+#define dbg(expr) dbg_impl_(expr, \
 	_Generic((expr), \
 		signed char: "%hhd\n", \
 		short: "%hd\n", \
